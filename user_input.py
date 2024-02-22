8581eb15f5ebf28adf440b0b9553ec6360bda52c
Donchagwa <davisonchagwa.com>
2024-02-22 17:25:15 +0300

    my first commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#Get the user's name and store it in variale name
+name=input("Enter your name: ")
+#Get the age of the user and store it in variable age
+age=input("Enter your age: ")
+#Get the location of the user and store it in variable location
+location=input("Enter your location: ")
+print("Hello {},you are {} years old and live in {}".format(name,age,location))
+
